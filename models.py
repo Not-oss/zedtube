d71c01cb31eddbb7bd8181df91b714e073c8ab63
zed <adem69d@gmail.com>
2025-03-27 01:28:04 +0100

    zezez

--- models.py
+++ models.py
@@ -8,11 +8,11 @@ db = SQLAlchemy()
 
 
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+def set_password(self, password):
+    self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+def check_password(self, password):
+    return check_password_hash(self.password_hash, password)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
